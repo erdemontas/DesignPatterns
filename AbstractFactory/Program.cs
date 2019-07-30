618f7594f9e43719b638cdac287d85cd7dca7cb5
Erdem ONTAS <erdem.ontas@windowslive.com>
2019-07-30 23:06:47 +0300

    Abstract Factory

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new Factory2());
+            productManager.GetAll();
+            Console.ReadLine();
+        }
+    }
+
+    public abstract class Logging
+    {
+        public abstract void Log(string message);
+    }
+
+    public class Log4NetLogger : Logging
+    {
+        public override void Log(string message)
+        {
+            Console.WriteLine("Logged with Log4Net");
+        }
+    }
+
+    public class NLogger : Logging
+    {
+        public override void Log(string message)
+        {
+            Console.WriteLine("Logged with NLogger");
+        }
+    }
+
+    public abstract class Caching
+    {
+        public abstract void Cache(string data);
+    }
+
+    public class MemCache : Caching
+    {
+        public override void Cache(string data)
+        {
+            Console.WriteLine("Cached with MemCache");
+        }
+    }
+    public class RedisCache : Caching
+    {
+        public override void Cache(string data)
+        {
+            Console.WriteLine("Cached with RedisCache");
+        }
+    }
+
+    //We created a factory which is abstract. It shows us that there can be more factories
+    public abstract class CrossCuttingConcersFactory
+    {   //In this factory, we will create an object for the logging or caching or both of them.
+
+        public abstract Logging CreateLogger();
+        public abstract Caching CreateCaching();
+    }
+
+    public class Factory1 : CrossCuttingConcersFactory
+    {
+        //According to business logic, you can create whichever factory you want
+        public override Caching CreateCaching()
+        {
+            return new RedisCache();
+        }
+
+        public override Logging CreateLogger()
+        {
+            return new Log4NetLogger();
+        }
+    }
+
+
+    public class Factory2 : CrossCuttingConcersFactory
+    {
+        //According to business logic, you can create whichever factory you want
+        public override Caching CreateCaching()
+        {
+            return new MemCache();
+        }
+
+        public override Logging CreateLogger()
+        {
+            return new NLogger();
+        }
+    }
+
+    //Implement business logic
+    public class ProductManager
+    {
+        private CrossCuttingConcersFactory _crossCuttingConcernsFactory;
+        private Logging _logging;
+        private Caching _caching;
+
+        public ProductManager(CrossCuttingConcersFactory crossCuttingConcernsFactory)
+        {
+            _crossCuttingConcernsFactory = crossCuttingConcernsFactory;
+            _logging = _crossCuttingConcernsFactory.CreateLogger();
+            _caching = _crossCuttingConcernsFactory.CreateCaching();
+        }
+        public void GetAll()
+        {
+            _logging.Log("Logged");
+            _caching.Cache("Data");
+            Console.WriteLine("All Product Listed");
+        }
+
+    }
+}

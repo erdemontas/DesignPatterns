4b3d9272e3afcae71c1b4e14eb6d10846f83d524
Erdem ONTAS <erdem.ontas@windowslive.com>
2019-08-08 00:22:44 +0300

    Prototype and Builder has been Added

---
+++ Builder/Program.cs
@@ -0,0 +1,106 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductDirector director = new ProductDirector();
+            var builder = new OldCustomerProductBuilder();
+
+            director.GenerateBuilder(builder);
+            var model = builder.GetModel();
+            Console.WriteLine(model.ProductName);
+            Console.WriteLine(model.Id);
+            Console.WriteLine(model.CategoryName);
+            Console.WriteLine(model.UnitPrice);
+            Console.WriteLine(model.DiscountedPrice);
+
+            Console.ReadLine();
+            
+        }
+    }
+
+    class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal DiscountedPrice { get; set; }
+        public bool DiscountApplied { get; set; }
+    }
+
+    //ViewModeli Üretecek Builder
+    abstract class ProductBuilder
+    {//Why Abstract? --> You need to make easier to switch builder sources when it's necessary
+        public abstract void GetProductData();
+        public abstract void AppliedDiscount();
+        public abstract ProductViewModel GetModel();
+    }
+
+    class NewCustomerProductBuilder : ProductBuilder
+    {
+        ProductViewModel model = new ProductViewModel();
+
+        public override void AppliedDiscount()
+        {
+            
+            model.DiscountedPrice = model.UnitPrice *(decimal)0.90;
+            model.DiscountApplied = true;
+        }
+
+        public override void GetProductData()
+        {
+            //Assume you get those values from database
+            model.Id = 1;
+            model.ProductName = "Chai";
+            model.CategoryName = "Beverage";
+            model.UnitPrice = 20;
+        }
+
+        public override ProductViewModel GetModel()
+        {
+            return model;
+        }
+    }
+
+    class OldCustomerProductBuilder : ProductBuilder
+    {
+        ProductViewModel model = new ProductViewModel();
+        public override void AppliedDiscount()
+        {
+            model.DiscountedPrice = model.UnitPrice;
+            model.DiscountApplied = false;
+        }
+
+        public override void GetProductData()
+        {
+            //Assume you get those values from database
+            model.Id = 1;
+            model.ProductName = "Chai";
+            model.CategoryName = "Beverage";
+            model.UnitPrice = 20;
+        }
+
+        public override ProductViewModel GetModel()
+        {
+            return model;
+        }
+    }
+
+     class ProductDirector
+    {
+        public void GenerateBuilder(ProductBuilder productBuilder)
+        {
+            productBuilder.GetProductData();
+            productBuilder.AppliedDiscount();
+        }
+    }
+
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer
+            {
+                Id = 1,
+                FirstName = "Erdem",
+                LastName = "Ontas",
+                City = "Istanbul"
+            };
+
+            Customer customer2 = (Customer)customer1.Clone();
+            customer2.FirstName = "Salih";
+
+            //These lines tries to prove that customer1 and customer2 are totaly different objects. 
+            //Clone() method makes new object not copy the same
+            //You reduce the cost of making new reference 
+            Console.WriteLine(customer1.FirstName);
+            Console.WriteLine(customer2.FirstName);
+
+
+            Console.ReadLine();
+        }
+    }
+
+    public abstract class Person
+    {
+        public abstract Person Clone();
+ 
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class Customer : Person
+    {
+        public string City { get; set; }
+
+        public override Person Clone()
+        {//This helps to clone Customer object
+            return (Person)MemberwiseClone();
+        }
+
+        public class Employee : Person
+        {
+            public decimal Salary { get; set; }
+
+            public override Person Clone()
+            {//This helps to clone Customer object
+                return (Person)MemberwiseClone();
+            }
+        }
+    }
+}
